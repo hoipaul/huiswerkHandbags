5d996c598edd73b993b2f0f4ed203b54fe915cbc
Paul Hermans <paulhermans@MacBook-Air-van-Paul.local>
2021-05-21 15:46:58 +0200

    buttons toegevoegd

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import Button from './Button';
 import './App.css';
 
 function App() {
   return (
+      <>
       <h1>Handbags & Purses</h1>
+  <nav>
+    <Button
+        title="to the collection"
+        type="button"
+        onClick={() => console.log()}/>
+    <Button
+        title="shop all the bags"
+        type="button"
+        onClick={() => console.log()}/>
+    <Button
+        title="pre-orders"
+        type="button"
+        onClick={() => console.log()}/>
+  </nav>
+        </>
   );
 }
 
